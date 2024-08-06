99feed1918af8a29c50b1f9909f8a524f377a6ac
chinogenio8 <cristobalmaier1@gmail.com>
2024-08-06 19:33:01 -0300

    update boton cv

---
+++ script.js
@@ -0,0 +1,9 @@
+const menuBtn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+
+menuBtn.addEventListener('click', () => {
+  menuBtn.classList.toggle('active');   
+
+  menu.classList.toggle('active');   
+
+});
